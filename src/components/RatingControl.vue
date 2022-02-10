e06f6b35d908a4391d125a312169e06d0c8ccc24
jemis-maru <jemis.maru@bacancy.com>
2022-02-10 18:41:01 +0530

    Complete section-11.

---
+++ src/components/RatingControl.vue
@@ -0,0 +1,58 @@
+<template>
+    <ul>
+        <li :class="{active: modelValue=='poor'}">
+            <button type="button" @click="selectOption('poor')">Poor</button>
+        </li>
+        <li :class="{active: modelValue=='avg'}">
+            <button type="button" @click="selectOption('avg')">Average</button>
+        </li>
+        <li :class="{active: modelValue=='great'}">
+            <button type="button" @click="selectOption('great')">Great</button>
+        </li>
+    </ul>
+</template>
+
+<script>
+    export default {
+        props: ['modelValue'],
+        emits: ['update:modelValue'],
+        methods:{
+            selectOption(option){
+                this.$emit('update:modelValue', option);
+            },
+        }
+    }
+</script>
+
+<style scoped>
+.active{
+    border-color: #4169e1;
+}
+
+.active button{
+    color: #4169e1;
+}
+
+ul{
+    list-style: none;
+    margin: 0.5rem 0;
+    padding: 0;
+    display: flex;
+}
+
+li{
+    margin: 0 1rem;
+    border: 1px solid #ccc;
+    padding: 1rem;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+button{
+    font: inherit;
+    background-color: transparent;
+    border: none;
+    cursor: pointer;
+}
+</style>
---
+++ src/components/TheForm.vue
@@ -0,0 +1,173 @@
+<template>
+  <form @submit.prevent="submitFormData">
+    <div class="form-control" :class="{invalid: userNameValidity === 'invalid'}">
+      <label for="user-name">Your Name</label>
+      <input id="user-name" name="user-name" type="text" v-model.trim="formDataObj.userName" @blur="validateUserName" />
+      <p v-if="userNameValidity === 'invalid'">Invalid input: please enter some text!</p>
+    </div>
+    <div class="form-control">
+      <label for="age">Your Age (Years)</label>
+      <input id="age" name="age" type="number" v-model="formDataObj.age" />
+    </div>
+    <div class="form-control">
+      <label for="referrer">How did you hear about us?</label>
+      <select id="referrer" name="referrer" v-model="formDataObj.referrer">
+        <option value="google">Google</option>
+        <option value="wom">Word of mouth</option>
+        <option value="newspaper">Newspaper</option>
+      </select>
+    </div>
+    <div class="form-control">
+      <h2>What are you interested in?</h2>
+      <div>
+        <input id="interest-news" value="news" name="interest" type="checkbox" v-model="formDataObj.interest" />
+        <label for="interest-news">News</label>
+      </div>
+      <div>
+        <input id="interest-tutorials" value="tutorials" name="interest" type="checkbox" v-model="formDataObj.interest" />
+        <label for="interest-tutorials">Tutorials</label>
+      </div>
+      <div>
+        <input id="interest-nothing" value="nothing" name="interest" type="checkbox" v-model="formDataObj.interest" />
+        <label for="interest-nothing">Nothing</label>
+      </div>
+    </div>
+    <div class="form-control">
+      <h2>How do you learn?</h2>
+      <div>
+        <input id="how-video" value="video courses" name="how" type="radio" v-model="formDataObj.how" />
+        <label for="how-video">Video Courses</label>
+      </div>
+      <div>
+        <input id="how-blogs" value="blogs" name="how" type="radio" v-model="formDataObj.how" />
+        <label for="how-blogs">Blogs</label>
+      </div>
+      <div>
+        <input id="how-other" value="other" name="how" type="radio" v-model="formDataObj.how" />
+        <label for="how-other">Other</label>
+      </div>
+    </div>
+    <div class="form-control">
+      <rating-control v-model="formDataObj.rating"></rating-control>
+    </div>
+    <div>
+      <button>Save Data</button>
+    </div>
+  </form>
+</template>
+
+<script>
+import RatingControl from './RatingControl.vue';
+
+export default {
+  components: {
+    'rating-control': RatingControl,
+  },
+  data(){
+    return{
+      formDataObj: {
+        userName: '',
+        age: null,
+        referrer: '',
+        interest: [],
+        how: '',
+        rating: '',
+      },
+      userNameValidity: 'pending',
+    };
+  },
+  methods:{
+    submitFormData(){
+      console.log(this.formDataObj);
+      this.formDataObj = {
+        userName: '',
+        age: null,
+        referrer: '',
+        interest: [],
+        how: '',
+        rating: '',
+      };
+    },
+    validateUserName(){
+      if(this.formDataObj.userName === ''){
+        this.userNameValidity = 'invalid';
+      }
+      else{
+        this.userNameValidity = 'valid';
+      }
+    },
+  },
+}
+</script>
+
+<style scoped>
+form {
+  margin: 2rem auto;
+  max-width: 40rem;
+  border-radius: 12px;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+  padding: 2rem;
+  background-color: #ffffff;
+}
+
+.form-control {
+  margin: 0.5rem 0;
+}
+
+.form-control.invalid input {
+  border-color: #ff0000;
+}
+
+.form-control.invalid label, .form-control.invalid p {
+  color: #ff0000;
+}
+
+label {
+  font-weight: bold;
+}
+
+h2 {
+  font-size: 1rem;
+  margin: 0.5rem 0;
+}
+
+input,
+select {
+  display: block;
+  width: 100%;
+  font: inherit;
+  margin-top: 0.5rem;
+}
+
+select {
+  width: auto;
+}
+
+input[type='checkbox'],
+input[type='radio'] {
+  display: inline-block;
+  width: auto;
+  margin-right: 1rem;
+}
+
+input[type='checkbox'] + label,
+input[type='radio'] + label {
+  font-weight: normal;
+}
+
+button {
+  font: inherit;
+  border: 1px solid #0076bb;
+  background-color: #0076bb;
+  color: white;
+  cursor: pointer;
+  padding: 0.75rem 2rem;
+  border-radius: 30px;
+}
+
+button:hover,
+button:active {
+  border-color: #002350;
+  background-color: #002350;
+}
+</style>
